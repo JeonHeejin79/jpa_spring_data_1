b0edc99e8791f74722f2d2e86a22ccff466b326b
SKMNS\202110043 <jeonheejin@sk.com>
2023-07-10 17:07:07 +0900

    commit_20230710

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,16 +1,20 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
+@RequiredArgsConstructor // 스프링부트가 지원해준다.
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+//    @PersistenceContext
+//    private EntityManager em;
+
+    private final EntityManager em;
 
     // 저장
     public void save(Member member) {
@@ -19,7 +23,18 @@ public class MemberRepository {
 
     // 조회
     public Member findOne(Long id) {
-        Member member = em.find(Member.class, id);
-        return member;
+        return em.find(Member.class, id);
+    }
+
+    // 리스트 조회 - jpql 사용
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 조회할때 성능을 더 최적화 한다.
+@RequiredArgsConstructor // final 로 선언된 필드로 생성자를 만들어준다.
+public class MemberService {
+
+    // 생성자 Injection 이 좋다. (filed injection, setter injection, constructor injection)
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // 쓰기에는 resOnlyTrue 를 넣지 않는다.
+    public Long join(Member member) {
+        validationDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validationDuplicateMember(Member member) {
+        // EXCEPTION
+        // 멀티스레드 환경에서 동시 접근을 방어하기위해 db 에 name 을 유니크제약조건을 걸어두는게 좋다.
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
