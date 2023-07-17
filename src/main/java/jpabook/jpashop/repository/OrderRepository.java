84eccf5a9182ae7545b4a026754b957188f2ae84
SKMNS\202110043 <jeonheejin@sk.com>
2023-07-18 08:57:17 +0900

    commit_20230718

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // JPQL
+    public List<Order> findAll(OrderSearch orderSerarch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSerarch.getOrderStatus())
+                .setParameter("name", orderSerarch.getMemberName())
+                // .setFirstResult(100) 페이징
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
+    }
+}
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional // 기본적으로 rollback 을한다. @Rollback(false) 로 설정필요
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    // @Rollback(false)
+    public void 회원가입() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        em.flush(); // 영속성컨텍스트 안에 있는 쿼리를 날린다.
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+    
+    }
+    
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2); // 예외가 발생해야 한다.
+        });
+    }
+}
